63c3a23baac8400ace39f944febd057247a517f4
keGZ <kegz@mail.ru>
2017-05-24 15:07:56 +0800

    initial commit

--- card.rb
+++ card.rb
@@ -1,5 +1,5 @@
 # blin codirovci
-card = ["6", "7", "8", "9", "10", "‚ «Ąâ", "„ ¬ ", "Š®ŕ®«ě", "’ă§"]
-mast = ["—Ąŕ˘ë", "ăˇ­ë", "Ź¨Ş¨", "’ŕĄäë"]
+card = ["6", "7", "8", "9", "10", "Valet", "Dama", "Korol", "Tuz"]
+mast = ["Cherv", "Bubni", "Tref", "Piki"]
 
 puts "#{card.sample} #{mast.sample}"
